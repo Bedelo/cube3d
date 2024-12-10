0c07669417b2e663f21f1f63726393e8b6203b87
Home <s_ceb@hotmail.com>
2024-12-10 15:29:45 +0100

    change map to array

--- srcs/map_to_array.c
+++ srcs/map_to_array.c
@@ -7,11 +7,14 @@ char	**fill_empty_map(t_map_creation **map, int *k)
 	t_map_creation	*m;
 
 	m = *map;
-	while (*k < m->dim[0])
+	printf("before fill K = %d\n", *k);											//* printer
+	while (*k < m->dim[1])
 	{
+		ft_putstr_fd("COMPLETE LINE \n", 1);										//* printer
 		m->my_map[m->i][*k] = '0';
 		*k += 1;
 	}
+	m->my_map[m->i][*k] = 0;
 	return (m->my_map);
 }
 
@@ -21,15 +24,23 @@ char	**fill_map(t_map_creation **map, int *k)
 
 	m = *map;
 	*k = 0;
-	m->j = 0;
 	m->line = get_next_line(m->fd);
-	while (*k < ft_strlen(m->line))
+	while (*k <= ft_strlen(m->line))
 	{
-		m->my_map[m->i][*k] = m->line[*k];
 		if ((m->line[*k] == 'N' || m->line[*k] == 'S' || m->line[*k] == 'W' || m->line[*k] == 'E') && m->flag)
 			return (NULL);
 		if ((m->line[*k] == 'N' || m->line[*k] == 'S' || m->line[*k] == 'W' || m->line[*k] == 'E') && !m->flag)
+		{
+			m->my_map[m->i][*k] = m->line[*k];
 			m->flag = 1;
+		}
+		if (m->line[*k] == '1' || m->line[*k] == '0')
+			m->my_map[m->i][*k] = m->line[*k];
+		if (m->line[*k] == ' ')
+			m->my_map[m->i][*k] = '0';
+		if (m->line[*k] == 0)
+			fill_empty_map(&m, k);
+			// return (m->my_map);
 		*k += 1;
 	}
 	ft_putchar_fd('\n', 1);								//* printer
@@ -45,19 +56,21 @@ char	**create_map(t_map_creation **map)
 	m->flag = 0;
 	m->i = 0;
 	ft_putstr_fd("first loop\n", 1);										//* printer
-	while (m->i < m->dim[1])
+	while (m->i < m->dim[0])
 	{
 		ft_putstr_fd("fill_map test?\n", 1);							//* printer
 		if (!fill_map(&m, &k))
 			return (shield_malloc_2(m->my_map, m->dim[0]));
-		if (k != m->dim[1])
-			if (!fill_empty_map(&m, &k))
-				return (shield_malloc_2(m->my_map, m->dim[0]));
-		free(m->line);									//*check if it is correct
+		printf("k after fill map = %d\n", k);											//* printer
+		// if (k < m->dim[1])
+		// 	if (!fill_empty_map(&m, &k))
+		// 		return (shield_malloc_2(m->my_map, m->dim[0]));
+		free(m->line);
 		m->line = NULL;
 		m->i += 1;
 		k = 0;
 		ft_putstr_fd("fill_next OK\n", 1);							//* printer
+		printf("###################################\n");											//* printer
 	}
 	return (m->my_map);
 }
@@ -71,7 +84,7 @@ int	ft_initialise_map(t_map_creation **map)
 	if (m->fd == -1)
 		return (1);
 	ft_putstr_fd("enter to create map OK\n", 1);				//* printer
-	m->my_map = create_map(&m);
+	create_map(&m);
 	close(m->fd);
 	if (!m->my_map)
 		return (1);
@@ -90,12 +103,12 @@ t_map_creation	*ft_map(t_map_creation **map, char **av)
 	map_dim(&i, m->file, len_x);
 	m->dim[1] = i;
 	i = 0;
-	m->my_map = ft_calloc(m->dim[0] + 1 , sizeof(char *));
+	m->my_map = ft_calloc(m->dim[0] , sizeof(char *));
 	if (!m->my_map)
 		return (shield_malloc(m->my_map));
 	while (i < m->dim[0])
 	{
-		m->my_map[i] = ft_calloc(m->dim[1] + 1, sizeof(char));
+		m->my_map[i] = ft_calloc(m->dim[1], sizeof(char));
 		if (!m->my_map)
 			return (shield_malloc_2(m->my_map, m->dim[0]));
 		i++;
@@ -109,10 +122,10 @@ t_map_creation	*ft_map(t_map_creation **map, char **av)
 	if (!i)
 	{
 		int j = 0;
-		while (i < m->dim[0]-1)
+		while (i < m->dim[0])
 			{
 
-				while (j < m->dim[1] - 1)
+				while (j < m->dim[1])
 				{
 
 					ft_putchar_fd(m->my_map[i][j], 1);
